c4f27d6d022641d155f5a310dfff3752cf32bf80
Srini Karri <yourgitemail@example.com>
2022-06-24 22:47:08 -0700

    power of 2 program

---
+++ Test.java
@@ -0,0 +1,19 @@
+class Test {
+    
+    public static void main(String[] args) {
+
+        int base = 2;
+        int exponant = 4;
+        int product= 1;
+        
+        for(int i=1;i<=exponant;i++)
+        {
+            product= product * base;
+            System.out.println(product); 
+            
+
+        }
+
+
+    }
+}
