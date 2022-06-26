6c7f131e515ea5ffe2aa0805506fba10364b540f
Srini Karri <yourgitemail@example.com>
2022-06-26 16:23:38 -0700

    Nested loops

--- App.java
+++ App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[]args){
-        //ForLoopPractice.Print_Ten_Numbers();
-        //MathUtil.Pow_Of_X(2, 4);
-        whileLoop.Check_if_number_is_in_range(1,100);
+        ForLoopPractice.Nested_Loop_Sample('|',10,20);
+        //MathUtil.Pow_Of_X(2, 4);""
+        //whileLoop.Check_if_number_is_in_range(1,100);
+
        
 
 
--- ForLoopPractice.java
+++ ForLoopPractice.java
@@ -1,4 +1,13 @@
 public class ForLoopPractice {
+public static void Nested_Loop_Sample(char symbol,int rows, int column)
+{
+    for (int k= 1; k <=rows; k++)
+    {
+        for (int i = 1; i <=column;i++)
+            System.out.print(symbol);
+        System.out.println();
+    }
+}
  public static void Print_Ten_Numbers()
  {
     for (int i=0;i<=10;i++)
@@ -8,3 +17,4 @@ public class ForLoopPractice {
 
  }
 }
+
