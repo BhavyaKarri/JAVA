2aca13a1d4e6a9e126337ce39fc94094f542218d
Srini Karri <yourgitemail@example.com>
2022-06-26 15:49:00 -0700

    extended while loop hehe:)

--- whileLoop.java
+++ whileLoop.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class whileLoop {
+
+    public static void Check_if_number_is_in_range(int rangeStart,int rangeEnd )
+    {
+        while (true)
+        {
+            System.out.println("To terminate the loop please enter an integer within the givin range "+ rangeStart+ " : "+rangeEnd);
+            Scanner Reader = new Scanner (System.in);
+            int number = Reader.nextInt();
+            if(number >= rangeStart && number <= rangeEnd){
+                System.out.println("Please enter an integer Value ");
+                System.out.println(number+" is in range!:) "+ rangeStart+":"+rangeEnd);
+                break;
+
+            }
+            
+        }
+
+    }
+    
+
     public static void Check_if_number_is_in_range(int number,int rangeStart,int rangeEnd )
     {
         while (number >= rangeStart && number <= rangeEnd)
@@ -6,7 +28,7 @@ public class whileLoop {
             System.out.println(number+" is in range!:) "+ rangeStart+":"+rangeEnd);
             return;
         }
-        System.out.println(number+"is not in range!:(( "+ rangeStart+":"+rangeEnd);
+        System.out.println(number+" is not in range!:(( "+ rangeStart+":"+rangeEnd);
 
     }
     
