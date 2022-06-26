1394f0106647502c59312a412e37df5a8cc57ef2
Srini Karri <yourgitemail@example.com>
2022-06-26 11:35:14 -0700

    For loop Pratice

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[]args){
-        System.out.println("Hello world");
+        ForLoopPractice.Print_Ten_Numbers();
     }
     
 }
---
+++ ForLoopPractice.java
@@ -0,0 +1,10 @@
+public class ForLoopPractice {
+ public static void Print_Ten_Numbers()
+ {
+    for (int i=0;i<=10;i++)
+    {
+        System.out.println(i);
+    }
+
+ }
+}
