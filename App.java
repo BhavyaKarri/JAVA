209a06218edb79c8637790b4aead5570daad738f
Srini Karri <yourgitemail@example.com>
2022-07-10 10:47:23 -0700

    While loop Practice

--- App.java
+++ App.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[]args){
-        ForLoopPractice.Nested_Loop_Sample('|',10,20);
+        //ForLoopPractice.Nested_Loop_Sample('|',10,20);
         //MathUtil.Pow_Of_X(2, 4);""
         //whileLoop.Check_if_number_is_in_range(1,100);
-
-       
-
-
+        
+        final int SENTINEl = -999;
+        Scanner Reader = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Please enter value:");
+            int value = Reader.nextInt();
+            if(value != SENTINEl)
+            {
+                System.out.println("processing value="+ value );
+            }
+            else
+            {
+                break;
+            }
+        }              
+        Reader.close(); 
     }
     
 }
--- whileLoop.java
+++ whileLoop.java
@@ -31,5 +31,28 @@ public class whileLoop {
         System.out.println(number+" is not in range!:(( "+ rangeStart+":"+rangeEnd);
 
     }
+
+    public static void Practice1(String[]args){
+        //ForLoopPractice.Nested_Loop_Sample('|',10,20);
+        //MathUtil.Pow_Of_X(2, 4);""
+        //whileLoop.Check_if_number_is_in_range(1,100);
+        
+        final int SENTINEl = -999;
+        Scanner Reader = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Please enter value:");
+            int value = Reader.nextInt();
+            if(value != SENTINEl)
+            {
+                System.out.println("processing value="+ value );
+            }
+            else
+            {
+                break;
+            }
+        }              
+        Reader.close(); 
+    }
     
 }
